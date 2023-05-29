21ff38b3491f9cc71e88325dd5f1a9b03109ef3d
John Reyes <jhonfer96@hotmail.com>
2023-05-29 00:19:15 -0500

    add winning condition

--- App.js
+++ App.js
@@ -28,7 +28,32 @@ export default function App() {
       }
       setBoard(newBoard)
       setRefresh(!refresh)
+      checkIfPlayerWon()
     }
+  }
+
+  const checkIfPlayerWon = () => {
+    if (board[0] == board[1] && board[1] == board[2] && board[0] != " ") {
+      playWon(board[0])
+    } else if (board[3] == board[4] && board[4] == board[5] && board[3] != " ") {
+      playWon(board[3])
+    } else if (board[6] == board[7] && board[7] == board[8] && board[6] != " ") {
+      playWon(board[6])
+    } else if (board[0] == board[4] && board[4] == board[8] && board[0] != " ") {
+      playWon(board[0])
+    } else if (board[2] == board[4] && board[4] == board[6] && board[2] != " ") {
+      playWon(board[2])
+    } else if (board[0] == board[3] && board[3] == board[6] && board[0] != " ") {
+      playWon(board[0])
+    } else if (board[1] == board[4] && board[4] == board[7] && board[1] != " ") {
+      playWon(board[1])
+    } else if (board[2] == board[5] && board[5] == board[8] && board[2] != " ") {
+      playWon(board[2])
+    }
+  }
+
+  const playWon = () => {
+
   }
 
   return (
