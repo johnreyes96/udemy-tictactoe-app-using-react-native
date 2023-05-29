f748e0a9b686aa50ebc076d1e8eb8e6adeead84c
John Reyes <jhonfer96@hotmail.com>
2023-05-28 23:46:55 -0500

    creating the board

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [ notification, setNotification ] = react.useState("Player X to start!")
+  const [ refresh, setRefresh ] = react.useState(false)
+  const [ board, setBoard ] = react.useState(
+    [
+      " ", " ", " ",
+      " ", " ", " ",
+      " ", " ", " "
+    ]
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.txt1}>TicTacToe</Text>
+      <Text style={styles.txt2}>{notification}</Text>
+      <FlatList
+        style={styles.list}
+        data={board}
+        numColumns={3}
+        refreshing={true}
+        extraData={refresh}
+        renderItem={({item, index}) =>
+          <TouchableOpacity style={styles.square}>
+            <Text>{index}</Text>
+          </TouchableOpacity>
+        }
+      />
     </View>
   );
 }
@@ -15,6 +39,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  txt1: {
+    fontSize: 50
+  },
+  txt2: {
+    fontSize: 20
+  },
+  list: {
+    width: 300,
+    height: 400
+  },
+  square: {
+    height: 60,
+    width: 30,
+    backgroundColor: 'red',
+    margin: 10
+  }
 });
